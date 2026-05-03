2b8f786033eabc5f44fd1136f003c40b20170b4f
Nga Duong <annhienduong2102@gmail.com>
2026-05-03 18:26:05 +0700

    first commit

---
+++ menu.js
@@ -0,0 +1,182 @@
+const menu = [
+  {
+    type: "folder",
+    title: "CB138",
+    children: [
+      {
+        type: "folder",
+        title: "lesson-2",
+        children: []
+      },
+      {
+        type: "folder",
+        title: "lesson-4",
+        children: []
+      },
+      {
+        type: "folder",
+        title: "lesson-6",
+        children: []
+      },
+      {
+        type: "folder",
+        title: "lesson-8",
+        children: []
+      },
+      {
+        type: "folder",
+        title: "lesson-10",
+        children: [
+{
+            type: "item",
+            title: "exercise-1",
+            file: "data/CB138/cb138_l10_ex1.json"
+          },
+{
+            type: "item",
+            title: "exercise-2",
+            file: "data/CB138/cb138_l10_ex2.json"
+          },
+{
+            type: "item",
+            title: "exercise-3",
+            file: "data/CB138/cb138_l10_ex3.json"
+          },
+          {
+            type: "item",
+            title: "exercise-4",
+            file: "data/CB138/cb138_l10_ex4.json"
+          },
+ {
+            type: "item",
+            title: "exercise-5",
+            file: "data/CB138/cb138_l10_ex5.json"
+          },
+{
+            type: "item",
+            title: "exercise-6",
+            file: "data/CB138/cb138_l10_ex6.json"
+          },
+        ]
+      },
+      {
+        type: "folder",
+        title: "lesson-12",
+        children: [
+{
+            type: "item",
+            title: "ex-1",
+            file: "data/CB138/cb138_l12_ex1.json"
+          },
+{
+            type: "item",
+            title: "ex-2",
+            file: "data/CB138/cb138_l12_ex2.json"
+          },
+{
+            type: "item",
+            title: "ex-3",
+            file: "data/CB138/cb138_l12_ex3.json"
+          },
+          {
+            type: "item",
+            title: "ex-4",
+            file: "data/CB138/cb138_l12_ex4.json"
+          },
+ {
+            type: "item",
+            title: "ex-5",
+            file: "data/CB138/cb138_l12_ex5.json"
+          },
+ {
+            type: "item",
+            title: "warmup-1",
+            file: "data/CB138/cb138_l12_wup1.json"
+          },
+ {
+            type: "item",
+            title: "warmup-2",
+            file: "data/CB138/cb138_l12_wup2.json"
+          },
+]
+      },
+{
+        type: "folder",
+        title: "lesson-14",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-16",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-18",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-20",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-22",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-24",
+        children: []
+      },
+{
+        type: "folder",
+        title: "lesson-26",
+        children: []
+      },
+    ]
+  },
+
+  {
+    type: "folder",
+    title: "dang-section-1",
+    children: [
+{
+            type: "item",
+            title: "01",
+            file: "data/dang-section-1/01.json"
+          },
+]
+  },
+
+  {
+    type: "folder",
+    title: "dang-section-2",
+    children: []
+  },
+
+  {
+    type: "folder",
+    title: "dang-section-3",
+    children: [
+{
+            type: "item",
+            title: "01",
+            file: "data/dang-section-3/01.json"
+          },
+]
+  },
+
+  {
+    type: "folder",
+    title: "dang-section-4",
+    children: []
+  },
+
+  {
+    type: "folder",
+    title: "dang-dac-biet",
+    children: []
+  }
+];
